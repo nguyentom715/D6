5b76956a45989def0fc0dab0232778a436b34799
nguyentom715 <nguyentom715@gmail.com>
2019-12-18 18:03:47 -0800

    Add files via upload

---
+++ agreement_login/js/popup.js
@@ -0,0 +1,27 @@
+/**
+ * Add button with id for changing logout.
+ */
+$(document).ready(function() {
+  var baseUrl = Drupal.settings.baseUrl;
+  var isPopup = Drupal.settings.isPopup;
+  if (isPopup === true) {
+      $("#myModal").modal({
+        backdrop: false
+      });
+      var logoutbtn = `${baseUrl}/logout`;
+
+      $("#btnAgree").click(function() {
+        $.ajax({
+          type: "POST",
+          url: "?q=mymodule_ajax",
+          data: {}
+        }).done(function() {
+          $("#myModal").modal("hide");
+          isPopup = false;
+        });
+      });
+      $("#btnDisagree").click(function() {
+        window.location.href = logoutbtn;
+      });
+    }
+});
